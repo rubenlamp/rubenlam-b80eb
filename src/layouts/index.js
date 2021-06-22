3b817f32fb81d834b6c567ce1d0286f7b43f1224
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:23:26 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import contact from './contact';
 import home from './home';
 import page from './page';
 import post from './post';
-
+import project from './'
 export { contact, home, page, post,project };
 
 export default {
