13d397bc28ffe1d33b827bec1ac6170b38d7b4ea
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:11:05 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -94,7 +94,7 @@ export default class Home extends React.Component {
         const hasMoreLink = _.get(page, 'has_more_link');
         const moreLinkText = _.get(page, 'more_link_text');
         const posts = _.orderBy(_.get(this.props, 'posts', []), 'date', 'desc');
-        const projects
+        const projects = _.orderBy(_.get(this.props, 'projects', []), 'date', 'desc');
         return (
             <Layout page={page} config={config}>
                 <Header config={config} page={page} image={headerImage} />
