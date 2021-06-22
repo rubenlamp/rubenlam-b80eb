e74bfcddc8c51d66e0d7d6a720c6ae64c5aaa4a3
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:54:06 +0000

    getPageUrl.js: changed

--- src/utils/getPageUrl.js
+++ src/utils/getPageUrl.js
@@ -5,7 +5,7 @@ export default function getPostUrl(post, { withPrefix: addPrefix = false } = {})
     const urlPath = _.get(post, '__metadata.urlPath');
     return addPrefix ? withPrefix(urlPath) : urlPath;
 }
-export default function getPageUrl(project, { withPrefix: addPrefix = false } = {}) {
+export default function getProjectUrl(project, { withPrefix: addPrefix = false } = {}) {
     const urlPath = _.get(project, '__metadata.urlPath');
     return addPrefix ? withPrefix(urlPath) : urlPath;
 }
