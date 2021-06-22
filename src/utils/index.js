cc2b4d3ad3d977f653e0d5ea3f34d8141f8daea1
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:56:14 +0000

    index.js: changed

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,7 @@ export { default as getPage } from './getPage';
 export { default as getData } from './getData';
 export { default as htmlToReact } from './htmlToReact';
 export { default as getPostUrl } from './getPageUrl';
+export { default as getPostUrl } from './getPageUrl';
 export { default as pathJoin } from './pathJoin';
 export { default as markdownify } from './markdownify';
 export { default as withPrefix } from './withPrefix';
