44b8712a598bbccdd50a69188a910a353992ecd8
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:06:39 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -46,7 +46,7 @@ export default class Home extends React.Component {
             </article>
         );
     }
-     renderProyect(post, index, hasMoreLink, moreLinkText) {
+     renderProject(project, index, hasMoreLink, moreLinkText) {
         const title = _.get(post, 'title');
         const thumbImage = _.get(post, 'thumb_img_path');
         const thumbImageAlt = _.get(post, 'thumb_img_alt', '');
