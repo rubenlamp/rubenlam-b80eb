253a2b0b9ad583e969eacbf34aa3fe930f528082
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 22:52:50 +0000

    getPageUrl.js: changed

--- src/utils/getPageUrl.js
+++ src/utils/getPageUrl.js
@@ -4,7 +4,7 @@ import withPrefix from './withPrefix';
 export default function getPageUrl(post, { withPrefix: addPrefix = false } = {}) {
     const urlPath = _.get(post, '__metadata.urlPath');
     return addPrefix ? withPrefix(urlPath) : urlPath;
-    
+}
 export default function getPageUrl(project, { withPrefix: addPrefix = false } = {}) {
     const urlPath = _.get(project, '__metadata.urlPath');
     return addPrefix ? withPrefix(urlPath) : urlPath;
